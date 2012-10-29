b5128d4029037022378958895f4879380dd336c4
Mangin.Alexander <mywork.koder@gmail.com>
2012-10-29 22:45:27 +0600

    add abs Model, change link to qunit
    1) В конструкторе клонируются все поля из переданного ему объекта
    //Todo Не уверен что так правильно.. стоит еще подумать
    2) В set-ре устанавливается поля <=>
    1. Поле у объекта существует
    2. Значение поля отлично от undefined
    3) В get-ре поле вовращается <=>
    1. Оно существует у объекта
    2. Имя поля имеет тип строку.

---
+++ Task/Model.js
@@ -0,0 +1,36 @@
+var Model = function (data) {
+    var nameField;
+    for(nameField in data) {       
+            this[nameField] = data[nameField];       
+    }
+};
+/**
+ * @param {Object} attributes
+ *
+ * @example
+ *     item.set({title: "March 20", content: "In his eyes she eclipses..."});
+ */
+Model.prototype.set = function (attributes) {
+    var nameAttr;
+    for(nameAttr in attributes) {
+        if(attributes.hasOwnProperty(nameAttr)) {
+            if (typeof this[nameAttr] !== "undefined") {
+                this[nameAttr] = attributes[nameAttr];
+            }
+        }
+    }
+};
+/**
+ * @param {String} attribute
+ */
+Model.prototype.get = function (attribute) {
+    if (typeof attribute !== 'string' || typeof this[attribute] === "undefined") {
+        return; //return undefined;
+    }
+    return this[attribute];
+};
+/**
+ * @param {Object} attributes
+ */
+Model.prototype.validate = function (attributes) {throw new Error('this is Abstract method')};
+// Другие необходимые вам поля
---
+++ Task/ModelTest.js
@@ -0,0 +1,32 @@
+﻿module("Create model")
+test('Проверка конструктора клонирования', function () {
+    "use strict";    
+    var model = new Model(new TestObject(1));
+    equal(typeof model["myNumber"], "number", "Личный объект не скопировался");
+    equal(typeof model["commonNumber"], "number", "Объекты прототипа не скопировались");
+});
+test('Проверка метода get()', function () {
+    "use strict";
+    var model = new Model(new TestObject(1));    
+    equal(model.get("commonNumber"),20,"Нет доступа к полям прототипа");
+    equal(model.get("myNumber"),1,"Нет доступа к личным полям");
+    equal(typeof model.get({}),"undefined","Объект не исполняет интерфейс");
+    equal(typeof model.get("ТакогоПоляТочноНет"),"undefined","несуществующее поле определено");
+});
+test('Проверка метода set()', function () {
+    "use strict";
+    var model = new Model(new TestObject(1));
+    model.set({"myNumber": 2});
+    equal(typeof model.get("myNumber"), "number", "Присваение произошло с ошибкой");
+    equal(model.get("myNumber"), 2, "Присвоение произошло с ошибкой");
+    model.set({"commonNumber":2});
+    equal(TestObject.prototype.commonNumber, 20, "Присвоение испортило прототип");
+    equal(model.get("commonNumber"), 2, "Ошибка при присвоении наследуемому полю через прототип");
+});
+
+function TestObject (number) {
+        this.myNumber = number;
+    };
+TestObject.prototype.commonNumber = 20;
+
+
