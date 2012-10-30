ea754b65a01915f2ea2ce6cdc8072e6863f8bd1f
Mangin.Alexander <mywork.koder@gmail.com>
2012-10-31 01:22:15 +0600

    Добавил еще тестов на Event
    Так же освоил тесты на throws

--- Task/CreateEventTest.js
+++ Task/CreateEventTest.js
@@ -5,8 +5,27 @@
         return month + "/" + day + "/" + year;
     }, currentTime = new Date(), testEvent = new Event({"start":currentTime});
     equal(dateToString(testEvent.start), dateToString(currentTime));
-    testEvent = new Event({"start": new Date(2), "end": new Date(1)});
-    ok(testEvent.start.getTime() < testEvent.end.getTime());
     testEvent = new Event({"start": new Date(1), "end": new Date(2)});
     ok(testEvent.start.getTime() < testEvent.end.getTime());
+    
+    throws(
+        function (){new Event({"start": new Date(2), "end": new Date(1)})},
+        'Error("Даты начала и конца перепутаны")'
+    );
+    throws(
+        function (){new Event({"cost":-1})},
+        'Error("Цена за вход не может быть отрицательной")'
+    );
+    throws(
+        function (){new Event({"parties":"NoArray"})},
+        'Error("Участники - это массив")'
+    );
+    throws(
+        function (){new Event({"parties":["sds"]})},        	
+        'Error("У одного из участников нет поля <ИМЯ>")'
+    );
+    equal(new Event({"stars":2}).stars, 2,"При присваивании звезд произошла ошибка" );
+    equal(new Event({"stars":2.1}).stars, 2,"Функция устанавливающая звездочки не сработала" );    
+    
+    
 });
