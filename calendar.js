75db2deb08d60dd7b13a6e81f5e3766c811cc5c4
meded <1>
2012-10-31 11:58:07 +0600

    запилил методы для колеции

--- calendar.js
+++ calendar.js
@@ -6,6 +6,40 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var slice = Array.prototype.slice;
+function partial(fn) {
+    var args = slice.call(arguments, 1);
+    return function () {
+        return fn.apply(this, args.concat(slice.call(arguments)));
+    };
+}
+function isMore(a,b) {
+    return a > b;
+}
+function isLess(a,b) {
+    return a < b;
+}
+function isEqual(a,b) {
+    return a === b;
+}
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+var nowDate = new Date
+var MoreNow = partial(isMore,nowDate);
+var LessNow = partial(isLess,nowDate);
+
+function searchAnArray(array,str) {
+    var Equal = partial(isEqual,str);
+    return array.reduce(
+        function (sum, x) {
+            return sum+Equal(x)
+
+        },0
+    )
+}
 
 /**
  * Описания собития
@@ -24,69 +58,8 @@
  *
  * @return {Object}
  */
-//function Event(eventOptions) {
-//    'use strict';
-//    /**
-//     * проверка и приведения к дате
-//     * @param time {Number | Date} Дата или стока
-//     * @return {Date}              Вернет в формате даты
-//     */
-//    function isData(time){
-//        if ('string' === typeof(time)){
-//            var timeToData = new Date(time)
-//            if ('Invalid Date' === timeToData){
-//                new TypeError("не верный формат даты");
-//            } else{
-//                return timeToData
-//            }
-//        } else{
-//            return time
-//        }
-//}
-//    eventOptions.startEvent = isData(eventOptions.startEvent);
-//    eventOptions.endEvent = isData(eventOptions.endEvent);
-//    if (eventOptions.startEvent > eventOptions.endEvent) {
-//        var startEvent = eventOptions.startEvent;
-//        eventOptions.startEvent = eventOptions.endEvent;
-//        eventOptions.endEvent = startEvent;
-//    }
-//
-//    eventOptions.startEvent = eventOptions.startEvent || new Date;
-//    eventOptions.endEvent = eventOptions.endEvent || new Date;
-//    eventOptions.name = eventOptions.name || "Событие";
-//    eventOptions.description = eventOptions.description || "";
-//    eventOptions.tegs = eventOptions.tegs || undefined;
-//
-//    /**
-//     * преоброзумем адрес в точные кардинаты
-//     * @param place {string}    Адрес собыитя
-//     * @return {*}              Кардинаты провдения собития
-//     */
-//    function findCoordinates(place) {
-//        //...
-//        return coordinates;
-//    }
-//    if (eventOptions.coordinates === "" && eventOptions.place) {
-//        eventOptions.coordinates = findCoordinates(eventOptions.place);
-//    }
-//    eventOptions.place = +eventOptions.place || "";
-//    eventOptions.coordinates = +eventOptions.coordinates || "";
-//
-//
-//    var regColorcode = /^(#)?([0-9a-fA-F]{3})([0-9a-fA-F]{3})?$/;
-//    if (regColorcode.test(eventOptions.color) !== false) {
-//        eventOptions.color = "";
-//        new TypeError("все плохо это не цвет");
-//    }
-//    eventOptions.color = eventOptions.color || "#fff";
-//    eventOptions.reminders = eventOptions.reminders || false;
-//    eventOptions.reminderTimeBeforeEvent = isData(eventOptions.reminderTimeBeforeEvent) || eventOptions.startEvent;
-//    eventOptions.friends = eventOptions.friends || undefined;
-//    return eventOptions;
-//};
-
 var EventModel = Backbone.Model.extend({
-    default: {
+    defaults: {
         startEvent: new Date,
         endEvent: new Date,
         name:  "Событие",
@@ -100,10 +73,9 @@ var EventModel = Backbone.Model.extend({
         friends:  undefined
     },
     initialize: function() {
-        console.log();
-//        options.model.bind("Error",function(){
-//            console.log(1);
-//        },this)
+        this.bind("error", function (Error) {
+            console.log(Error);
+        })
     },
     errorList:function(){
         console.log(1);
@@ -111,11 +83,10 @@ var EventModel = Backbone.Model.extend({
     validate: function(attrs){
         var Error = {};
         function isData(time,attr){
-            Error[attr]= 10;
             if ('string' === typeof(time)){
                 var timeToData = new Date(time)
                 if ('Invalid Date' === timeToData){
-//                    new TypeError("не верный формат даты");
+                    Error[attr]= "не верный формат даты";
                     return timeToData
                 }
             } else{
@@ -124,238 +95,196 @@ var EventModel = Backbone.Model.extend({
         }
         attrs.startEvent = isData(attrs.startEvent,"startEvent");
         attrs.endEvent = isData(attrs.endEvent,"endEvent");
-        if(!_.isEmpty(Error)) {
-//        console.log(_.isEmpty(Error));
-//            console.log(Error);
-            return Error
+
+        var regColorcode = /^(#)?([0-9a-fA-F]{3})([0-9a-fA-F]{3})?$/;
+        if (regColorcode.test(attrs.color) !== false) {
+            attrs.color = "";
+            Error.color = "все плохо это не цвет";
         }
 
-    },
-    events:{
-        error: 'errorList'
+        if(!_.isEmpty(Error)) {
+            return Error
+        } else return false;
     }
-
 });
 
-event = new EventModel;
-event.set("startEvent","werfasdf")
-//function getRandomInt (min, max) {
-//    return Math.floor(Math.random() * (max - min + 1)) + min;
-//}
-//
-//var eventList = [];
-//function generTestCollection() {
-//    for (var i = 0; i < 30; i++) {
-//        var eventOptions = {};
-//
-//        function rendomTime () {
-//            var newTime = new Date;
-//            newTime.setMonth(newTime.getMonth() - getRandomInt(0,1));
-//            newTime.setDate(getRandomInt(1,31));
-//            newTime.setHours(getRandomInt(0,24));
-//            newTime.setMinutes(getRandomInt(0,59));
-//            return newTime
-//        }
-//        eventOptions.startEvent = rendomTime();
-//        eventOptions.endEvent = rendomTime();
-//
-//        eventOptions.name =  "Событие N" + i;
-//
-//        function giberArow() {
-//            var tegs = [];
-//            if (getRandomInt(0,1)){
-//                tegs.push("Программирование")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("Веб-разработка")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("Информационная безопасность")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("JavaScript")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("Фриланс")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("DIY или Сделай Сам")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("Android")
-//            }
-//            if (getRandomInt(0,1)){
-//                tegs.push("Linux")
-//            }
-//            return tegs.toString();
-//        }
-//        eventOptions.tegs = giberArow();
-//        eventOptions.place = "г Екатеринбург";
-//        eventOptions.coordinates = "56.828561,60.602373";
-//
-//        function giberColor() {
-//            var numberColor = getRandomInt(0,4);
-//            if (numberColor === 0){
-//                return "#fff"
-//            }
-//            if (numberColor === 1){
-//                return "#F75050"
-//            }
-//            if (numberColor === 2){
-//                return "#5566E7"
-//            }
-//            if (numberColor === 3){
-//                return "#4DC743"
-//            }
-//            if (numberColor === 4){
-//                return
-//            }
-//        }
-//        eventOptions.color = giberColor();
-//        eventOptions.reminders = !!getRandomInt(0,4);
-//        eventOptions.reminderTimeBeforeEvent = rendomTime();
-//
-//        function giberFriends() {
-//            var trgs = [];
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/alex_eho")
-//            }
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/id29476786")
-//            }
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/ostornfirst")
-//            }
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/chbgp")
-//            }
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/id20061985")
-//            }
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/id126316413")
-//            }
-//            if (getRandomInt(0,1)){
-//                trgs.push("http://vk.com/id31116305")
-//            }
-//            return trgs
-//        }
-//        eventOptions.friends = giberFriends();
-//        eventList.push(new Event(eventOptions))
-//    }
-//}
-//generTestCollection();
-//console.log("вся колеция");
-//console.log(eventList);
-//
-//
-//
-//function prop(name){
-//    return function (object){
-//        return  object[name];
-//    }
-//}
-//function func(name){
-//    return function (object){
-//        return  object[name]();
-//    }
-//}
-//var slice = Array.prototype.slice;
-//function partial(fn) {
-//    var args = slice.call(arguments, 1);
-//    return function () {
-//        return fn.apply(this, args.concat(slice.call(arguments)));
-//    };
-//}
-//function isMore(a,b) {
-//    return a > b;
-//}
-//function isLess(a,b) {
-//    return a < b;
-//}
-//function isEqual(a,b) {
-//    return a === b;
-//}
-//
-//var nowDate = new Date
-//var MoreNow = partial(isMore,nowDate);
-//var LessNow = partial(isLess,nowDate);
-//
-//function searchAnArray(array,str) {
-//    var Equal = partial(isEqual,str);
-//    return array.reduce(
-//        function (sum, x) {
-//            return sum+Equal(x)
-//
-//        },0
-//    )
-//}
-//
-//
-//// Выбрать прошедшие события
-//var endEvintListNow = eventList.filter(function (item) {
-//       return LessNow(item.endEvent)
-//    }
-//);
-//console.log("Выбрать прошедшие события");
-//console.log(endEvintListNow);
-//
-//
-//// Выбрать идушие сейчас
-//var EvinteNow = eventList.filter(function (item) {
-//       return LessNow(item.startEvent)
-//    }
-//).filter(function (item) {
-//        return MoreNow(item.endEvent)
-//    }
-//);
-//console.log("Выбрать идушие сейчас");
-//console.log(EvinteNow);
-//
-//
-////Выбрать события с моим участием  http://vk.com/alex_eho
-//var withAlex_eho = eventList.filter(function (item) {
-//    return searchAnArray(item.friends,"http://vk.com/alex_eho")
-//})
-//console.log("Выбрать события с моим участием  http://vk.com/alex_eho");
-//console.log(withAlex_eho);
-//
-////Выбрать мероприятия проходяшие в одно вемя
-//var atOneTime = eventList.map(function (item, index, array) {
-//
-//    var MoreThisItemStart = partial(isMore,item.startEvent);
-//    var LessThisItemEnd = partial(isLess,item.endEvent);
-//    var EqualThisItemEnd = partial(isEqual,item.endEvent);
-//    return array
-//        .map(function (el, index, array) {
-//                if (MoreThisItemStart(el.startEvent)){
-//                    return el
-//                }
-//            }
-//        )
-//        .map(function (el) {
-//            if (!el) return
-//            if (LessThisItemEnd(el.endEvent)){
-//                return el
-//            }
-//        }
-//    );
-//})
-//console.log("Выбрать мероприятия проходяшие в одно вемя");
-//console.log(atOneTime);
-//
-////сортировка по старту мероприятий
-//var sortStartEvinte = eventList.sort(function (a,b) {
-//    return 0+isMore( a.startEvent.getTime(), b.startEvent.getTime())
-//})
-//console.log("исходный масив");
-//eventList.forEach(function (item) {
-//    console.log(item.startEvent)
-//    }
-//)
-//console.log("сортировка по старту мероприятий");
-//sortStartEvinte.forEach(function (item) {
-//    console.log(item.startEvent)
-//    }
-//)
-//
+var EventList = Backbone.Collection.extend({
+    model: EventModel,
+    initialize:function () {
+        this.bind("all", function (nemeEvent) {
+            console.log(nemeEvent);
+        })
+        this.bind("cheng", function () {
+            console.log(this);
+        })
+    },
+    evemts:{
+        "add": function(model, collection) {
+            console.log(collection);
+        }
+    },
+    endEvintListNow: function () {
+        return new EventList(this.filter(function (item) {
+            return LessNow(item.get("endEvent"))
+        }))
+    },
+    startEventMoreNow: function () {
+        return new EventList(this.filter(function (item) {
+            return MoreNow(item.get("startEvent"))
+        }))
+    },
+    evinteNow: function () {
+        return new EventList(this.filter(function (item) {
+                return LessNow(item.get("startEvent"))
+            }
+        ).filter(function (item) {
+                return MoreNow(item.get("endEvent"))
+            }
+        ))
+    },
+    withFriends:function (nemeFriends) {
+        return new EventList(this.filter(function (item) {
+            return searchAnArray(item.get("friends"),nemeFriends)
+        }))
+    },
+    withAlex_eho: function () {
+        return new EventList(this.filter(function (item) {
+            return searchAnArray(item.get("friends"),"http://vk.com/alex_eho")
+        }))
+    },
+    atOneTime:  function () {
+        return new EventList(this.map(
+            function (item, index, array) {
+
+                var MoreThisItemStart = partial(isMore,item.get("startEvent"));
+                var LessThisItemEnd = partial(isLess,item.get("endEvent"));
+                var EqualThisItemEnd = partial(isEqual,item.get("endEvent"));
+                return array
+                    .map(function (el, index, array) {
+                        if (MoreThisItemStart(el.get("startEvent"))){
+                            return el
+                        }
+                    })
+                    .map(function (el) {
+                        if (!el) return
+                        if (LessThisItemEnd(el.get("endEvent"))){
+                            return el
+                        }
+                    });
+            }
+        ))
+    },
+    sortStartEvinte:function () {
+        return new EventList(this.sort(function (a,b) {
+            return 0+isMore( a.get("startEvent").getTime(), b.get("startEvent").getTime())
+
+        }))
+    }
+});
+
+eventList = new EventList;
+function generTestCollection() {
+    for (var i = 0; i < 30; i++) {
+        var eventOptions = {};
+
+        function rendomTime () {
+            var newTime = new Date;
+            newTime.setMonth(newTime.getMonth() - getRandomInt(0,1));
+            newTime.setDate(getRandomInt(1,31));
+            newTime.setHours(getRandomInt(0,24));
+            newTime.setMinutes(getRandomInt(0,59));
+            return newTime
+        }
+        eventOptions.startEvent = rendomTime();
+        eventOptions.endEvent = rendomTime();
+
+        eventOptions.name =  "Событие N" + i;
+
+        function giberArow() {
+            var tegs = [];
+            if (getRandomInt(0,1)){
+                tegs.push("Программирование")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("Веб-разработка")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("Информационная безопасность")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("JavaScript")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("Фриланс")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("DIY или Сделай Сам")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("Android")
+            }
+            if (getRandomInt(0,1)){
+                tegs.push("Linux")
+            }
+            return tegs.toString();
+        }
+        eventOptions.tegs = giberArow();
+        eventOptions.place = "г Екатеринбург";
+        eventOptions.coordinates = "56.828561,60.602373";
+
+        function giberColor() {
+            var numberColor = getRandomInt(0,4);
+            if (numberColor === 0){
+                return "#fff"
+            }
+            if (numberColor === 1){
+                return "#F75050"
+            }
+            if (numberColor === 2){
+                return "#5566E7"
+            }
+            if (numberColor === 3){
+                return "#4DC743"
+            }
+            if (numberColor === 4){
+                return
+            }
+        }
+        eventOptions.color = giberColor();
+        eventOptions.reminders = !!getRandomInt(0,4);
+        eventOptions.reminderTimeBeforeEvent = rendomTime();
+
+        function giberFriends() {
+            var trgs = [];
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/alex_eho")
+            }
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/id29476786")
+            }
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/ostornfirst")
+            }
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/chbgp")
+            }
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/id20061985")
+            }
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/id126316413")
+            }
+            if (getRandomInt(0,1)){
+                trgs.push("http://vk.com/id31116305")
+            }
+            return trgs
+        }
+        eventOptions.friends = giberFriends();
+        eventList.push(new EventModel(eventOptions));
+    }
+}
+generTestCollection();
+console.log("вся колеция");
+console.log(eventList);
+
+
