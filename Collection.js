fad05756b0bde52cf470e5b85984bb4b69494d04
Sergey Zhigalov <sergey.zhigalov@gmail.com>
2012-11-07 17:56:51 +0600

    Унаследовал Event от Model
    Инициализация полей пока только через set

---
+++ Collection.js
@@ -0,0 +1,24 @@
+﻿var Collection = function (items) {
+
+};
+
+/**
+ * @return {Collection}
+ */
+Collection.prototype.add = function (model) {};
+/**
+ * @param {Function} selector
+ *
+ * @see https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/filter
+ *
+ * @example
+ *    new Collection().filter(function (item) {
+ *        return item.get('attendee').indexOf("me") !== -1;
+ *    });
+ * @return {Collection}
+ */
+Collection.prototype.filter = function (selector) {};
+/**
+ * @return {Collection}
+ */
+Collection.prototype.sortBy = function (fieldName) {};
---
+++ Model.js
@@ -0,0 +1,27 @@
+﻿var Model = function (attributes) {
+    for (var keyName in attributes) {
+        this[keyName] = attributes[keyName];
+    }
+};
+
+/**
+ * @param {Object} attributes
+ *
+ * @example
+ *     item.set({title: "March 20", content: "In his eyes she eclipses..."});
+ */
+Model.prototype.set = function (attributes) {
+    for (var keyName in attributes) {
+        this[keyName] = attributes[keyName];
+    }
+};
+/**
+ * @param {String} attribute
+ */
+Model.prototype.get = function (attribute) {
+    return this[attribute];
+};
+/**
+ * @param {Object} attributes
+ */
+Model.prototype.validate = function (attributes) { throw new Error('this is Abstract method') };
